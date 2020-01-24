d12f5e482db1ec7311a062021e771cb64370d6c3
Santosh Kumar <kasantosh@yahoo.com>
2020-01-24 11:33:33 -0500

    Opened files for MVC and basic search query works and sends results

--- src/js/index.js
+++ src/js/index.js
@@ -1,6 +1,4 @@
-// Global app controller
+import Search from './models/Search';
 
-import num from './test';
-
-const x = 23;
-console.log(`I imported ${num} from another module called test.js! Variable x is ${x}`);
+const search = new Search('pizza');
+search.getResults();

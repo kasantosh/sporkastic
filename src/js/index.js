2153b202b4be882b64ad8f544a99f5e891d3d2ec
Santosh Kumar <kasantosh@yahoo.com>
2020-01-29 10:40:10 -0500

    Working on the Recipe model

--- src/js/index.js
+++ src/js/index.js
@@ -1,4 +1,5 @@
 import Search from './models/Search';
+import Recipe from './models/Recipe';
 import { elements, renderLoader, clearLoader } from './views/base';
 import * as searchView from './views/searchView';
 
@@ -10,6 +11,7 @@ import * as searchView from './views/searchView';
 
 const state = {};
 
+// SEARCH CONTROLLER =====================
 const controlSearch = async () => {
     // 1. Get query from view
     const query = searchView.getInput();
@@ -48,3 +50,31 @@ elements.searchResPages.addEventListener('click', e => {
         searchView.renderResults(state.search.result, goToPage);
     }
 })
+
+// RECIPE CONTROLLER =====================
+const controlRecipe = async () => {
+
+    // Get ID from the URL
+    const id = window.location.hash.replace('#', '');
+    console.log(id);
+
+    if (id) {
+        // Prepate UI for changes
+
+        //  Create new Recipe Object
+        state.recipe = new Recipe(id);
+
+        // Get recipe data
+        await state.recipe.getRecipe();
+
+        // Calculate servings and time
+        state.recipe.calcTime();
+        state.recipe.calcServings();
+
+        // Render recipe
+        console.log(state.recipe);
+
+    }
+}
+
+window.addEventListener('hashchange', controlRecipe);
